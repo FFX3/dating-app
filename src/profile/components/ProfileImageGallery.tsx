7987293edc80d94e4229ee8b56178a4329923626
Justin McIntyre <justinmcintyre42@gmail.com>
2023-10-10 17:54:12 -0400

    created carousel

--- src/profile/components/ProfileImageGallery.tsx
+++ src/profile/components/ProfileImageGallery.tsx
@@ -1,10 +1,48 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, Image, Dimensions, StyleSheet } from 'react-native'
+import { useProfile } from '../contexts/profile'
 
 export default function ProfileImageGallery(){
+    const { profile: { gallery } } = useProfile()
+    
     return (
-        <View style={{ width: 50, height: 50, backgroundColor: 'green' }}>
-            <Text>Profile image gallery goes here</Text>
+            <FlatList data={gallery}
+                renderItem={({ item, index }) => {
+                    return <GalleryImageItem item={{
+                        uri: item
+                    }} />
+                }}
+                horizontal
+                pagingEnabled
+                snapToAlignment='center'
+            />
+    )
+}
+
+const { width, height } = Dimensions.get('screen')
+
+function GalleryImageItem({ item }){
+    return (
+        <View style={styles.container}>
+            <Image 
+                source={{ uri: item.uri }}
+                style={styles.image}
+                resizeMode='contain'
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width,
+        height: height * 0.6,
+        alignItems: 'center',
+    },
+
+    image: {
+        flex: 1,
+        width: '100%',
+    },
+
+})
--- src/profile/screens/EditGalleryScreen.tsx
+++ src/profile/screens/EditGalleryScreen.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { ScrollView, Button, Text, Image, View } from 'react-native';
 import { useProfile } from '../contexts/profile';
-import Carousel, { Pagination } from 'react-native-snap-carousel'
 
 export function EditGalleryScreen({ navigation }) {
     const { profile: { gallery } } = useProfile()
@@ -16,20 +15,6 @@ export function EditGalleryScreen({ navigation }) {
     return (
         <ScrollView>
             <View style={{ flex: 1, backgroundColor: 'black', alignItems: 'center' }}>
-                <Carousel
-                    layout='default'
-                    data={images}
-                    sliderWidth={300}
-                    itemWidth={300}
-                    renderItem={({ item, index }) => (
-                        <Image
-                            key={index}
-                            style={{ width: '100%', height: '100%' }}
-                            resizeMode='contain'
-                            src={item.image.uri}
-                        />
-                    )}
-                />
                 <Button title='save' onPress={save} />
             </View>
         </ScrollView>
