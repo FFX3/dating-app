a4d38a370077b61d5c82d23bc0b3cc12667b2b1a
Justin McIntyre <justinmcintyre42@gmail.com>
2023-10-20 21:58:25 -0300

    basic message bubbles

--- src/messager/screens/ThreadScreen.tsx
+++ src/messager/screens/ThreadScreen.tsx
@@ -1,4 +1,4 @@
-import { FlatList, View, Text } from "react-native"
+import { FlatList, View, Text, StyleSheet } from "react-native"
 import { useMessager, Message } from "../contexts/messager"
 import { useEffect } from "react"
 
@@ -15,15 +15,40 @@ export function ThreadScreen({ route }) {
     return (
         <FlatList
             data={messages}
-            renderItem={({ item } : { item: Message }) => item.sender ?
-                <View>
-                    <Text>{item.contents}</Text>
+            renderItem={({ item } : { item: Message }) =>(
+                <View style={styles.message_row}>
+                    <View style={item.sender ? styles.sent_message_bubble : styles.received_message_bubble}>
+                        <Text style={styles.message}>{item.contents}</Text>
+                    </View>
                 </View>
-                : 
-                <View>
-                    <Text>{item.contents}</Text>
-                </View>
-            }
+            )}
         />
     )
 }
+
+const styles = StyleSheet.create({
+    message_row: {
+        width: '100%',
+        padding: 10,
+        alignSelf: 'center',
+    },
+
+    sent_message_bubble: {
+        alignSelf: 'flex-end',
+        backgroundColor: 'green',
+        padding: 7,
+        borderRadius: 3,
+    },
+
+    received_message_bubble: {
+        alignSelf: 'flex-start',
+        backgroundColor: 'blue',
+        padding: 7,
+        borderRadius: 3,
+    },
+
+    message: {
+        color: 'white',
+        fontSize: 16,
+    }
+})
