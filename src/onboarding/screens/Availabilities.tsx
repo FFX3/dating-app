4824ac7e9a965e9d0944a48534d899a71b866024
Justin McIntyre <justinmcintyre42@gmail.com>
2023-11-13 13:50:07 -0400

    moved app folder added supabase

--- src/onboarding/screens/Availabilities.tsx
+++
@@ -1,6 +0,0 @@
-import AvailabilityNavigator from "../../availabilities/navigation/AvailabilityNavigator";
-
-export function Availability() {
-    
-    return <AvailabilityNavigator header={false} />
-}
--- src/utils/snowflake.ts
+++
@@ -1,9 +0,0 @@
-import SnowflakeId from 'snowflake-id'
-
-
-const snowflake = new SnowflakeId({
-    mid : 42,
-    offset : (2022-1970)*31536000*1000
-});
-
-export default snowflake
